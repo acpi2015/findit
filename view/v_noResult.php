8b1495a1d92824a5d6f1024b883991862967d398
alacommare <aurore.lacommare@etu.u-bordeaux.fr>
2014-12-05 01:04:40 +0100

    Delete models+create searchbycriteria+formNoResult

---
+++ view/v_noResult.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "No result. Would you like launch the search by criteria ?"
+
+ <form action=".php" method="post">  //complete action		
+ 	<input type="SUBMIT" value="Yes"/>
+ 	<input type="SUBMIT" value="No"/>
+ </form>
+
+
+?>
