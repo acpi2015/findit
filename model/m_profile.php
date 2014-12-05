29b77cc377686ed4eb72d02782ab28d0fd80d441
alacommare <aurore.lacommare@etu.u-bordeaux.fr>
2014-12-05 05:04:37 +0100

    create results

--- model/m_profile.php
+++ model/m_profile.php
@@ -1,3 +1,14 @@
 <?php
+	include "class.Person.php";
+
+
+	$person = new class Person("Aurore", "Laco", "/img/pict", 1, "small", "normal", "black", 0, 1);
+
+	$person = new Person($value["Aurore"],$value["Laco"],$value["photoPerson"],
+						$value["sexPerson"],$value["sizePerson"],$value["corpulencePerson"],$value["hairColorPerson"],
+						$value["tatooPerson"],$value["idOrganizationPerson"]);
+
+
+	echo $person.getFirstname();
 
 ?>
